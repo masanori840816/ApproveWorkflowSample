febeade764b616a5aef715a8dc94c0e8be07683c
Masui Masanori <masanori840816@gmail.com>
2021-02-08 07:32:42 +0900

    add approved date into workflow

--- ApprovementWorkflowSample/Approvements/Workflow.cs
+++ ApprovementWorkflowSample/Approvements/Workflow.cs
@@ -13,11 +13,13 @@ namespace ApprovementWorkflowSample.Approvements
         public int Id { get; init; }
         [Required]
         [Column("title")]
-        public string Title { get; init; } = "";
+        public string Title { get; set; } = "";
         [Required]
         [Column("workflow_type_id")]
         [ForeignKey("WorkflowType")]
-        public int WorkflowTypeId { get; init; }
+        public int WorkflowTypeId { get; set; }
+        [Column("approved_date", TypeName = "timestamp with time zone")]
+        public DateTime? ApprovedDate { get; set; }
         [Required]
         [Column("last_update_date", TypeName = "timestamp with time zone")]
         [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
---
+++ ApprovementWorkflowSample/Migrations/20210207223013_AddApproveedDateIntoWorkflow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ApprovementWorkflowSample.Migrations
+{
+    public partial class AddApproveedDateIntoWorkflow : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "approved_date",
+                table: "Workflow",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "approved_date",
+                table: "Workflow");
+        }
+    }
+}
